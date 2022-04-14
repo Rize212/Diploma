888c2a60094527923415fd263ba4155eaa10f628
Rize212 <afolk212@gmail.com>
2022-04-14 22:19:02 +0300

    v0.3

---
+++ Diploma/Pages/Shared/Log in.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Diploma.Pages
+{
+    public class Log_inModel : PageModel
+    {
+        private readonly ILogger<Log_inModel> _logger;
+
+        public Log_inModel(ILogger<Log_inModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
---
+++ Diploma/Pages/Shared/Registration.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Diploma.Pages.Shared
+{
+    public class RegistrationModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
