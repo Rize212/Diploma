889e5376e5e3558951798c18ecbd242a6b7274f6
Алексей Фолкуян <rize@MacBook-Pro-Aleksej.local>
2022-04-19 01:14:08 +0300

    First commit

---
+++ 222/Controllers/AccountController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace _222.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Log_in()
+        {
+            return View();
+        }
+        public IActionResult Registration()
+        {
+            return View();
+        }
+        public IActionResult Home()
+        {
+            return View();
+        }
+    }
+}
