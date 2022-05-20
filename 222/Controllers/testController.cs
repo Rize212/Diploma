48e912b2e8abd283d831c968374546c5009c4324
Rize212 <afolk212@gmail.com>
2022-05-21 01:25:00 +0300

    Zalupa penis her davalka hiy bledina

---
+++ 222/Controllers/testController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _222.Controllers
+{
+    public class testController : Controller
+    {
+        // GET: testController
+        public ActionResult test()
+        {
+            return View();
+        }
+
+
+    }
+}
