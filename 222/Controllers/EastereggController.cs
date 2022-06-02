8bab2db18b13ca1c5b01851ee3ce0b3c4bb9f093
Rize212 <afolk212@gmail.com>
2022-06-02 17:54:06 +0300

    qwerty

---
+++ 222/Controllers/EastereggController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _222.Controllers
+{
+    public class EastereggController : Controller
+    {
+        public IActionResult design() => View();
+       
+        public IActionResult programming() => View();
+    
+            
+    }
+}
